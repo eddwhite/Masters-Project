003d5d116cbb5df612192dd1ddfb14eb4368715e
eddwhite <eddwhite1@gmail.com>
2016-02-28 20:29:52 +0000

    Implemented FFMPEG lossless video output

--- input_handler.py
+++ input_handler.py
@@ -1,28 +1,60 @@
 # This takes an input video, converts it to B&W and scales it to the correct size
 
 import cv2
+import subprocess as sp
 import numpy as np
+import os
+
 
 def main():
-    input_video = 'test.avi'
-    output_video = 'output.avi'
-    output_size = (width, height)
+    input_video = 'far_away.mp4'
+    output_video = 'far_away_b&w.avi'
 
+    # OpenCV video input
     v_in = cv2.VideoCapture(input_video)
-    v_out = cv2.VideoWriter(output_video, -1, v_in.get('CV_CAP_PROP_FPS'), output_size)
+
+    # Get input video details
+    v_width = int(v_in.get(cv2.CAP_PROP_FRAME_WIDTH))
+    v_height = int(v_in.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    v_fps = v_in.get(cv2.CAP_PROP_FPS)
+
+    # FFMPEG Video output
+    FFMPEG_BIN = 'C:/ffmpeg/bin/ffmpeg.exe'
+    command = [FFMPEG_BIN,
+               '-y',  # (optional) overwrite output file if it exists
+               '-f', 'rawvideo',
+               '-vcodec', 'rawvideo',
+               '-s', '%dx%d'% (v_width, v_height),  # size of one frame
+               '-pix_fmt', 'rgb24',
+               '-r', '%.02f' % v_fps,  # frames per second
+               '-i', '-',  # The imput comes from a pipe
+               '-an',  # Tells FFMPEG not to expect any audio
+               '-vcodec', 'png',
+               output_video]
+
+    pipe = sp.Popen(command, stdout=open(os.devnull, 'wb'), stdin=sp.PIPE, stderr=None)
 
     while v_in.isOpened():
-        ret, f_in = v_in.read()
+        ret, frame = v_in.read()
 
-        f_in = cv2.cvtColor(f_in, cv2.COLOR_BGR2GRAY)  # convert to greyscale
-        f_in = cv2.adaptiveThreshold(f_in, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)  # b&w
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # convert to greyscale
+            frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)  # b&w
+            frame = cv2.resize(frame, (v_width, v_height), interpolation=cv2.INTER_CUBIC)  # resize
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
 
-        f_out = cv2.resize(f_in, output_size, interpolation=cv2.INTER_CUBIC)
-        v_out.write(f_out)
+            # Convert numpy array to character stream
+            if frame.dtype != np.uint8:
+                frame = frame.astype('uint8')
+            pipe.stdin.write(frame.tostring())
+        else:
+            break
 
-    v_out.release()
     v_in.release()
     cv2.destroyAllWindows()
+    pipe.stdin.close()
+    pipe.wait()
+
 
 if __name__ == "__main__":
     main()
--- route_to_video.py
+++ route_to_video.py
@@ -1,12 +1,17 @@
 import collections
+import os
 import numpy as np
 import cv2
 from flip_disc_display import FlipDiscDisplay
 import math
+import subprocess as sp
 
 
 def main():
-    # Initialise a display
+    # Initialise
+    v_width = 3*5;
+    v_height = 4*5;
+    output_video = 'output_test.avi'
     display = FlipDiscDisplay(4, 3, 5, 1)
 
     # Create a route
@@ -15,27 +20,43 @@ def main():
     for col in range(0, 3):
         v1.append(step('east', 1))
         v1.append(step('actuate', 0 * np.ones((5, 5, 3), np.uint8)))
-
     r = [v1]
 
     # Perform route and get length
     display.perform_route(r)
     video_length = math.ceil(display.get_route_time()) + 1
 
-    fps = 15
-    # Define the codec and create VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'DIB ')
-    out = cv2.VideoWriter('output.avi', -1, fps, (3*5, 4*5), 1)
+    v_fps = 15
+    # FFMPEG Video output
+    FFMPEG_BIN = 'C:/ffmpeg/bin/ffmpeg.exe'
+    command = [FFMPEG_BIN,
+               '-y',  # (optional) overwrite output file if it exists
+               '-f', 'rawvideo',
+               '-vcodec', 'rawvideo',
+               '-s', '%dx%d'% (v_width, v_height),  # size of one frame
+               '-pix_fmt', 'rgb24',
+               '-r', '%.02f' % v_fps,  # frames per second
+               '-i', '-',  # The imput comes from a pipe
+               '-an',  # Tells FFMPEG not to expect any audio
+               '-vcodec', 'png',
+               output_video]
+    pipe = sp.Popen(command, stdout=open(os.devnull, 'wb'), stdin=sp.PIPE, stderr=None)
 
-    for t in np.linspace(0, video_length, num = video_length * fps):
+    for t in np.linspace(0, video_length, num=video_length * v_fps):
         frame = display.realtime_animation(t)
-        out.write(frame)
+        # Write frame to file
+        if frame.dtype != np.uint8:
+                frame = frame.astype('uint8')
+        pipe.stdin.write(frame.tostring())
+
+        # Display video
         cv2.imshow('Simulation', frame)
-        if cv2.waitKey(1000 / fps) & 0xFF == ord('q'):
+        if cv2.waitKey(1000 / v_fps) & 0xFF == ord('q'):
             break
 
-    out.release()
     cv2.destroyAllWindows()
+    pipe.stdin.close()
+    pipe.wait()
 
 
 if __name__ == "__main__":
